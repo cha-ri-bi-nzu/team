f6ee81e7cc9e5d0833f7a81570725ccf7b08ec79
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-31 20:34:09 +0900

    オーナーのみチームの編集が出来る機能追加

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :are_you_owner, only: %i[edit update]
 
   def index
     @teams = Team.all
@@ -55,5 +56,9 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def are_you_owner
+    redirect_to team_path unless current_user == @team.owner
   end
 end
